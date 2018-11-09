6f23ebd5193f09e28ab26b86daf55afd484c837a
Likhita Narayana <likhitanarayana@yahoo.com>
2018-11-09 03:19:49 -0800

    Started tf-idf

---
+++ TF-IDF.py
@@ -0,0 +1,24 @@
+from os import listdir
+from os.path import isfile, join
+import os
+import sys
+import pandas as pd
+
+
+def tf_idf(root, output_file):
+    ground_truth = pd.DataFrame(columns = ['file_name', 'author'])
+    print("listdir = {}".format(listdir(root)))
+    author_folders = [f for f in listdir(root) if not isfile(join(root, f))]
+    files = []
+    for folder in author_folders:
+        print("folder = {}".format(folder))
+        new_folder = root +'/'+folder
+        folder_files = [f for f in listdir(new_folder) if isfile(join(new_folder, f))]
+        for f in folder_files:
+            ground_truth.loc[len(ground_truth)] = [f, folder]
+
+    print("ground_truth = {}".format(ground_truth))
+    print('files = {}'.format(files))
+
+
+tf_idf(sys.argv[1], sys.argv[2])
