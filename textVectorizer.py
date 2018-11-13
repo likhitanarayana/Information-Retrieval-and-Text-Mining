e59a88baa96c178874caee7380ca757a67ca0cf1
Likhita Narayana <likhitanarayana@yahoo.com>
2018-11-12 17:15:20 -0800

    removed limitation counter and be able to run tf-idf for all the files

--- textVectorizer.py
+++ textVectorizer.py
@@ -59,9 +59,6 @@ def tf_idf(root, output_file):
             tf = tf.drop(f, 1)
             #print("tf")
             #print(tf)
-        if counter >= 2:
-            break
-
 
 
     cols = list(tf)
