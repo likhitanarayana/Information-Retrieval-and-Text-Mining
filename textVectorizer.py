2e89fca53de97d0381fcbbabe5fef1bb23865e32
likhitanarayana <likhitanarayana@yahoo.com>
2018-11-14 20:01:58 -0800

    Add files via upload

--- textVectorizer.py
+++ textVectorizer.py
@@ -5,97 +5,128 @@ import sys
 import pandas as pd
 import string
 import numpy as np
+import pickle
 
 
-def individual_tf_idf(document_name):
-    """
-    Given the entire tf-idf csv file, only output the rows which contain words in the document asked for
-    :param document_name: Name of document which you want sparse vector representation of
-    :return:
-    """
-    # calculate tf-idf * tf for each word and turn them to an list for each document given and use numpy's dot product
-    raise NotImplementedError
+class Document:
+    def __init__(self):
+        self.length = -1
+        self.words = dict()
+
+    def __repr__(self):
+        return 'length = {}\n words = {}'.format(self.length, self.words)
+
+
+class Word_Information:
+    def __init__(self, tf, doc_frequency):
+        self.term_frequency = tf # number of times words shows up in this document
+        self.document_frequency = doc_frequency # number of documents the term shows up
+
+    def __repr__(self):
+        return 'term frequency = {} and doc frequency = {}'.format(self.term_frequency, self.document_frequency)
 
 
 
-def tf_idf(root, output_file):
+def tf_idf(root, stop_list_file=None):
+    # stop words list
+    if stop_list_file is not None:
+        stop_list = []
+        with open(stop_list_file) as fp:
+            for count, line in enumerate(fp):
+                #print("stop_word = {}".format(line))
+                #print("isalpha = {}".format(line.isalpha))
+                line = line.strip('\n')
+                if len(line) != 0:
+                    stop_list.append(line)
+
+    if stop_list is not None:
+        print("stop word = {}".format(stop_list))
+
+    doc_list = dict()
+
     ground_truth = pd.DataFrame(columns = ['file_name', 'author'])
-    tf = pd.DataFrame(columns = ['word'])
     author_folders = [f for f in listdir(root) if not isfile(join(root, f))]
     files = []
     counter = 0
     for folder in author_folders:
         print("folder = {}".format(folder))
+        print("folder # = {}".format(counter))
         new_folder = root +'/'+folder
         folder_files = [f for f in listdir(new_folder) if isfile(join(new_folder, f))]
         files.extend(folder_files)
         for f in folder_files:
-            counter += 1
+            #counter += 1
             print("file = {}".format(f))
             ground_truth.loc[len(ground_truth)] = [f, folder]
-            #f_words = []
-            #with open(new_folder+"/"+f) as open_f:
-            #    f_words = list(file.read().split())
+
             curr_file = open(new_folder+"/"+f, 'r')
             text = curr_file.read()
-            print("text = {}".format(text))
+            #print("text = {}".format(text))
             f_words = [word.strip(string.punctuation) for word in text.split()]
             f_words = [x.lower() for x in f_words]
             f_words = [word.replace(",", "") for word in f_words]
             f_words = [word.replace(".", "") for word in f_words]
             f_words = [word.replace("-", "") for word in f_words]
-            print("f-words = {}".format(f_words))
 
-
-            # print("f words = {}".format(f_words))
-            tf[f] = 0
+            doc_unique_words = list()
+            #print("f-words = {}".format(f_words))
+            doc_info = Document()
+            length = 0
             for word in f_words:
-                row_index = tf.loc[tf['word']==word].index
-                if len(row_index) != 0: # word already exists in dataframe
-                    tf.ix[row_index, f] += 1
-                else: # word does not exist yet in dataframe
-                    empty_list = [0] * len(tf.columns)
-                    tf.loc[len(tf)] = empty_list
-                    tf['word'][len(tf)-1] = word
-                    tf[f][len(tf)-1] = 1
-            tf[f+'_tf'] = tf[f]*1.0 / len(f_words)
-            tf = tf.drop(f, 1)
-            #print("tf")
-            #print(tf)
-            if counter >= 2:
+                if word not in stop_list:
+                    #print("word = {}".format(word))
+                    length += 1
+                    word_information = doc_info.words.get(word, None)
+                    #print("word information = {}".format(word_information))
+                    if word_information is None:
+                        # does not in document dictionary yet
+                        doc_info.words[word] = Word_Information(1, 1)
+                    else:
+                        # update word term frequency
+                        word_information.term_frequency += 1
+                        doc_info.words[word] = word_information # term frequency
+                    doc_unique_words.append(word)
+
+            # go through and update other documents frequencies
+
+            doc_unique_words = set(doc_unique_words)
+
+            #print("doc unique words = {}".format(doc_unique_words))
+            for word in doc_unique_words:
+                #print("word = {}".format(word))
+                # loop through all documents
+                num_found_in_other_documents = 0
+                #print("doc list = {}".format(doc_list))
+                for doc in doc_list:
+                    #print("doc = {}".format(doc))
+                    #print("document = {}".format(doc_list[doc]))
+                    doc_words = doc_list[doc]
+                    if word in doc_words.words:
+                        #print("original doc frequency = {}".format(doc_words.words[word].document_frequency))
+                        doc_words.words[word].document_frequency += 1
+                        #print("after doc frequency = {}".format(doc_words.words[word].document_frequency))
+                        num_found_in_other_documents += 1
+                #update document frequency for word in current document
+                #print("obj = {}".format(doc_info.words))
+                (doc_info.words[word]).document_frequency += num_found_in_other_documents
+
+            doc_list[f] = doc_info
+
+            if counter <= 2:
                 break
 
-
-    cols = list(tf)
-    cols.pop(0)
-    # tf['idf'] = np.log(len(files)*1.0/tf.eq(0).sum(axis=1))
-    tf['idf'] = np.nan
-    for index, row in tf.iterrows():
-        doc_num = 0
-        for col in cols:
-            if row[col] != 0:
-                doc_num += 1
-        num_files = len(tf.columns) - 2
-        print("len of files = {}".format(num_files))
-        print("index value = {}".format(index))
-        print("doc num = {}".format(doc_num))
-        tf.ix[index, 'idf'] = np.log(num_files * 1.0 / doc_num)
+    with open('objs.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+        pickle.dump(doc_list, f)
 
 
-    tf.to_csv('test.csv')
-    cols = list(tf)
-    cols.pop(0)
-    cols.pop()
-    for col in cols:
-        new_col = col+'-idf'
-        # print("col = {}".format(col))
-        tf[new_col] = tf[col].mul(tf['idf'])
-
-
-    # print("tf dataframe")
-    # print(tf)
     ground_truth.to_csv("ground_truth.csv")
-    tf.to_csv("test_tf-idf.csv")
+
+
+def test_reading_in_pickle(random):
+    with open('objs.pkl', 'rb') as f:
+        doc_vector = pickle.load(f)
+        print("doc vector = {}".format(doc_vector))
 
 
 tf_idf(sys.argv[1], sys.argv[2])
+#test_reading_in_pickle(sys.argv[1])
